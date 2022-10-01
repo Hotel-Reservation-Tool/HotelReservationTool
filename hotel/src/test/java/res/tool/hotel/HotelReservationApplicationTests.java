4bc63938fce94bc65fe8bd5a4bd1e3f19667b574
Oláh Beatrix <beatriix2002@gmail.com>
2022-10-01 11:31:54 +0200

    spring init

---
+++ hotel/src/test/java/res/tool/hotel/HotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package res.tool.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
