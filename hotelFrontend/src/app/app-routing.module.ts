169aa2ae2f3d5f3e532b4e886f800e108c143c39
kgyorgy4 <kallai4gyorgy@gmail.com>
2022-11-26 00:35:44 +0100

    gallery and contact page

--- hotelFrontend/src/app/app-routing.module.ts
+++ hotelFrontend/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
+import {GalleryComponent} from "./gallery/gallery.component";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {ContactComponent} from "./contact/contact.component";
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'home', component: MainPageComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
--- hotelFrontend/src/app/app.module.ts
+++ hotelFrontend/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MainPageComponent } from './main-page/main-page.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
