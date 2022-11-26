f839fdd103c27ec1a50b6f3b55e1772f6ec8e068
Oláh Beatrix <beatriix2002@gmail.com>
2022-11-26 18:14:19 +0100

    login/logout works

--- hotelFrontend/src/app/app.component.ts
+++ hotelFrontend/src/app/app.component.ts
@@ -4,7 +4,6 @@ import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {ReservationService} from "./book-room/reservation.service";
 import {Router} from "@angular/router";
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
 import {ClientService} from "./book-room/client.service";
 import {Client} from "./book-room/client";
 
@@ -24,9 +23,11 @@ export class AppComponent implements OnInit {
   phone: string = "";
   newRes: Reservation = new Reservation();
   public isLoggedIn: boolean;
+  public loggedInUSer: Client = null;
+  private badPw: boolean = false;
 
   constructor(private modalService: NgbModal, private fb: FormBuilder, private reservationService: ReservationService,
-              private router : Router) { }
+              private router : Router, private clientService: ClientService) { }
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
@@ -37,9 +38,16 @@ export class AppComponent implements OnInit {
     });
     this.clientService.getLoggedInClients().subscribe( logged => {
       this.isLoggedIn = logged.length != 0;
+      if(logged.length != 0){
+        this.loggedInUSer = logged[0];
+      }
   })
   }
    open(content: any) {
+     this.name = "";
+     this.email = "";
+     this.password = "";
+     this.phone = "";
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
@@ -57,30 +65,51 @@ export class AppComponent implements OnInit {
     }
   }
 
-  onSubmit() {
-    this.reservationService.getClientByEmail(this.email).subscribe(client => {
-      this.newRes.clientId = client.id;
-      this.reservationService.createReservation(this.newRes).subscribe();
-    })
-    this.router.navigate(['/home']);
-
-
   login(): void{
-    this.clientService.getClientByEmail("j@gmail.com").subscribe( c => {
-      c.isLoggedIn = 1;
-      this.clientService.updateClient(c.id, c).subscribe(
-      );
+    this.clientService.getClientByEmail(this.email).subscribe( c => {
+      if(c.password == this.password){
+        c.isLoggedIn = 1;
+        this.badPw = false;
+        this.clientService.updateClient(c.id, c).subscribe(
+        );
+      }
+      else{
+        alert("Bad password!")
+        this.badPw = true;
+      }
+    },
+      () => alert("Bad email or password!"),
+      () => {
+      if(!this.badPw){
+        window.location.reload()
+        this.name = "";
+        this.email = "";
+        this.password = "";
+        this.phone = "";
+      }
     })
   }
 
   logout(): void{
-    this.clientService.getClientByEmail("j@gmail.com").subscribe( c => {
+    this.clientService.getClientByEmail(this.loggedInUSer.email).subscribe( c => {
       c.isLoggedIn = 0;
       this.clientService.updateClient(c.id, c).subscribe();
+      this.loggedInUSer = null;
+      window.location.reload()
+      this.name = "";
+      this.email = "";
+      this.password = "";
+      this.phone = "";
     })
   }
 
-  register(client : Client){
+  register(){
+    let client:Client = new Client();
+    client.id = "asdasdsa";
+    client.name = this.name;
+    client.email = this.email;
+    client.password = this.password;
+    client.isLoggedIn = 0;
     this.clientService.createClient(client).subscribe();
 
   }
