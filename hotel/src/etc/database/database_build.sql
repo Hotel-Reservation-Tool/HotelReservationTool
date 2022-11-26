a2512d7626e6f476f07d94e952cc29e642a83093
xatixatix <xatixatix@gmail.com>
2022-11-26 11:27:43 +0100

    database_build.sql

--- hotel/src/etc/database/database_build.sql
+++ hotel/src/etc/database/database_build.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `hotel`.`client` (
-    `id` VARCHAR(100) NOT NULL,
-    `name` VARCHAR(45) NOT NULL,
-    `mobile` VARCHAR(12) NOT NULL,
-    `email` VARCHAR(45) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `mobile_UNIQUE` (`mobile` ASC) VISIBLE,
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+                                  `id` VARCHAR(100) NOT NULL,
+                                  `name` VARCHAR(45) NOT NULL,
+                                  `mobile` VARCHAR(12) NOT NULL,
+                                  `email` VARCHAR(45) NOT NULL,
+                                  PRIMARY KEY (`id`),
+                                  UNIQUE INDEX `mobile_UNIQUE` (`mobile` ASC) VISIBLE,
+                                  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8;
 
@@ -13,38 +13,40 @@ ALTER TABLE `hotel`.`client`
     ADD COLUMN `password` VARCHAR(45) NULL AFTER `email`;
 
 CREATE TABLE `hotel`.`room` (
-    `id` VARCHAR(100) NOT NULL,
-    `size` INT NOT NULL,
-    `max_ppl` INT NOT NULL,
-    `price` INT NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+                                `id` VARCHAR(100) NOT NULL,
+                                `size` INT NOT NULL,
+                                `max_ppl` INT NOT NULL,
+                                `price` INT NOT NULL,
+                                PRIMARY KEY (`id`),
+                                UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE `hotel`.`reservation` (
-    `roomid` VARCHAR(100) NOT NULL,
-    `clientid` VARCHAR(100) NOT NULL,
-    `start_date` DATE NOT NULL,
-    `end_date` DATE NOT NULL,
-    INDEX `clientid_idx` (`clientid` ASC) VISIBLE,
-    INDEX `roomid_idx` (`roomid` ASC) VISIBLE,
-    CONSTRAINT `clientid`
-        FOREIGN KEY (`clientid`)
-            REFERENCES `hotel`.`client` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `roomid`
-        FOREIGN KEY (`roomid`)
-            REFERENCES `hotel`.`room` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION)
+                                       `roomid` VARCHAR(100) NOT NULL,
+                                       `clientid` VARCHAR(100) NOT NULL,
+                                       `start_date` DATE NOT NULL,
+                                       `end_date` DATE NOT NULL,
+                                       `people` INT NOT NULL,
+                                       INDEX `clientid_idx` (`clientid` ASC) VISIBLE,
+                                       INDEX `roomid_idx` (`roomid` ASC) VISIBLE,
+                                       CONSTRAINT `clientid`
+                                           FOREIGN KEY (`clientid`)
+                                               REFERENCES `hotel`.`client` (`id`)
+                                               ON DELETE NO ACTION
+                                               ON UPDATE NO ACTION,
+                                       CONSTRAINT `roomid`
+                                           FOREIGN KEY (`roomid`)
+                                               REFERENCES `hotel`.`room` (`id`)
+                                               ON DELETE NO ACTION
+                                               ON UPDATE NO ACTION)
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8;
 
 ALTER TABLE `hotel`.`reservation`
     DROP FOREIGN KEY `roomid`;
 ALTER TABLE `hotel`.`reservation`
+    DROP COLUMN `people`,
     DROP COLUMN `roomid`,
     ADD COLUMN `reservationid` VARCHAR(100) NOT NULL FIRST,
     ADD PRIMARY KEY (`reservationid`),
@@ -52,19 +54,19 @@ ALTER TABLE `hotel`.`reservation`
 ;
 
 CREATE TABLE `hotel`.`reservation_helper` (
-    `reservationid` VARCHAR(100) NOT NULL,
-    `roomid` VARCHAR(100) NOT NULL,
-    INDEX `reservationid_idx` (`reservationid` ASC) VISIBLE,
-    CONSTRAINT `reservationid`
-        FOREIGN KEY (`reservationid`)
-            REFERENCES `hotel`.`reservation` (`reservationid`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-        CONSTRAINT `roomid`
-        FOREIGN KEY (`roomid`)
-            REFERENCES `hotel`.`room` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION);
+                                              `reservationid` VARCHAR(100) NOT NULL,
+                                              `roomid` VARCHAR(100) NOT NULL,
+                                              INDEX `reservationid_idx` (`reservationid` ASC) VISIBLE,
+                                              CONSTRAINT `reservationid`
+                                                  FOREIGN KEY (`reservationid`)
+                                                      REFERENCES `hotel`.`reservation` (`reservationid`)
+                                                      ON DELETE NO ACTION
+                                                      ON UPDATE NO ACTION,
+                                              CONSTRAINT `roomid`
+                                                  FOREIGN KEY (`roomid`)
+                                                      REFERENCES `hotel`.`room` (`id`)
+                                                      ON DELETE NO ACTION
+                                                      ON UPDATE NO ACTION);
 
 ALTER TABLE `hotel`.`client`
     ADD COLUMN `isLoggedIn` TINYINT NOT NULL DEFAULT 0 AFTER `password`,
